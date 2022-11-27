f9fb12167bd0f2426d91580540572c9ce40ff26f
wasia902 <117897579+wasia902@users.noreply.github.com>
2022-11-27 06:54:17 -0800

    Add files via upload

---
+++ uni.java
@@ -0,0 +1,25 @@
+public class uni  
+{  
+public static void main(String args[])   
+{  
+//it inserts a Tab Space  
+String str = "Andrew\tGarfield";  
+System.out.println(str);  
+//it inserts a New Line  
+String str1 = "the best way\nto communicate \nan idea \nis to act it out";  
+System.out.println(str1);  
+//it insert a backslash  
+String str2 = "And\\Or";  
+System.out.println(str2);  
+//it insert a Carriage  
+String str3 = "Carriage\rReturn";  
+System.out.println(str3);  
+//it prints a single quote  
+String str4 = "Wall Street\'s";  
+System.out.println(str4);  
+//it prints double quote  
+//String str5 = "New\'Twilight'Line";  
+String str5 = "'wasayumer'";  
+System.out.println(str5);  
+}  
+}  
